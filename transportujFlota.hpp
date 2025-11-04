a9aff3541672bbc90f77ca18763c5faf7654aa20
Michał Tomporowski <michalang400@gmail.com>
2025-11-04 23:41:48 +0100

    Poprawa

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -1,55 +1,7 @@
 #pragma once
 
 #include "Stocznia.hpp"
-#include <stdlib.h>
-#include <time.h>
-
-class Statek {
-public:
-	virtual ~Statek() {}
-	virtual unsigned int transportuj()
-	{
-		return 0;
-	}
-};
-
-class Tankowiec : public Statek {
-public:
-	unsigned int transportuj() override
-	{
-		return 5;
-	}
-};
-
-class Zaglowiec : public Statek {
-public:
-	unsigned int transportuj() override
-	{
-		return 1;
-	}
-};
-
-class Stocznia {
-public:
-	Stocznia()
-	{
-		srand(time(NULL));
-	}
-
-	Statek* operator()()
-	{
-		int min = 1;
-		int max = 2;
-		int L = max - min + 1;
-		selected = rand() % L + min;
-		if (selected == 2)
-			return new Tankowiec{};
-		else
-			return new Zaglowiec{};
-	}
-private:
-	double selected;
-};
+#include "Statki.hpp"
 
 unsigned int transportujFlota(unsigned int towar) {
 	if (towar == 0)
